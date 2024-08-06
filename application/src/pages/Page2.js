0415387f92dcaebca29a3b5f3035882828b99df1
Yash Nishikant <yash0312@hotmail.com>
2024-08-06 08:33:29 -0400

    template

---
+++ application/src/pages/Page2.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+
+export default function Page1() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar />
+    </LocalizationProvider>
+  );
+}
